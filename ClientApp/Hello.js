3822ca7c757b2457070b6c200daa18252c20d2e9
t.moechel <t.moechel@softcreation.de>
2019-06-26 22:40:54 +0200

    First Commit

---
+++ ClientApp/Hello.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>Hello React is working!!</div>
+        )
+    }
+}
+
+Hello.propTypes = {};
+Hello.defaultProps = {};
+
+export default Hello;
